2486fa37963c51f53443e83e7550ad1c754b0557
20211TIIMI0265 <ricardoleiteescola@gmail.com>
2023-05-03 12:44:35 -0300

    Passo08

--- app/src/main/java/rodrigues/leite/galeria/MainActivity.java
+++ app/src/main/java/rodrigues/leite/galeria/MainActivity.java
@@ -1,8 +1,14 @@
 package rodrigues.leite.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tbMain); //Obtemos o elemento
+        setSupportActionBar(toolbar); //indica que tbMain vai ser considerado a Action bar padrão
     }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb,menu);
+        return true;
+    }//Cria as opções de menu definidas no arquivo de menu
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){ //Sempre que a tollbar for clicada
+            case R.id.opCamera: //Caso o icone da camera seja clicado
+                dispatchTakePictureIntent(); //Dispara a camera
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+    }
+    }
 }
--- app/src/main/java/rodrigues/leite/galeria/PhotoActivity.java
+++ app/src/main/java/rodrigues/leite/galeria/PhotoActivity.java
@@ -1,8 +1,14 @@
 package rodrigues.leite.galeria;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Toolbar toolbar = findViewById(R.id.tbPhoto); //Obtemos o elemento
+        setSupportActionBar(toolbar); //indica que tbPhoto vai ser considerado a Action bar padrão
+
+        ActionBar actionBar = getSupportActionBar(); //Pega a ActionBar que definimos acima
+        actionBar.setDisplayHomeAsUpEnabled(true); //Habilita o botão de voltar
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb,menu);
+        return true;
+    } //Cria as opções de menu definidas no arquivo de menu
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare://quando o icone for clicado
+                sharePhoto();//excuta o codigo
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+            }
+        }
 }
